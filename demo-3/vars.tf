07cb91bf6b924691c4adfa09c9aa56a45d212ebd
Valentino Massimo <shouraigen@gmail.com>
2019-03-30 13:11:45 -0700

    terraform course files march 2019

--- demo-3/vars.tf
+++ demo-3/vars.tf
@@ -1,13 +1,13 @@
 variable "AWS_ACCESS_KEY" {}
 variable "AWS_SECRET_KEY" {}
 variable "AWS_REGION" {
-  default = "eu-west-1"
+  default = "us-west-2"
 }
 variable "AMIS" {
   type = "map"
   default = {
-    us-east-1 = "ami-13be557e"
-    us-west-2 = "ami-06b94666"
-    eu-west-1 = "ami-844e0bf7"
+    us-east-1 = "ami-020a9a7369c26052b"
+    us-west-2 = "ami-0b4a4b368aa8bd6f0"
+    eu-west-1 = "ami-08eb05c142193af92"
   }
 }
--- first-steps/instance.tf
+++ first-steps/instance.tf
@@ -1,10 +1,23 @@
 provider "aws" {
-  access_key = "ACCESS_KEY_HERE"
-  secret_key = "SECRET_KEY_HERE"
-  region     = "us-east-1"
+  access_key = "AKIA6G5T54T726RPWFU7"
+  secret_key = "OkFvmBVc77uUOAMVkFTcSmWJhxf9YpCbzWz2PpaC"
+  region     = "us-west-2"
 }
 
+
 resource "aws_instance" "example" {
-  ami           = "ami-0d729a60"
+  ami           = "ami-0b4a4b368aa8bd6f0"
   instance_type = "t2.micro"
+  }
+  resource "aws_vpc" "terraform" {
+  cidr_block = "10.0.0.0/16"
+  enable_dns_hostnames = true
+  tags {
+    Name = "production"
+  }
+}
+
+  resource "aws_subnet" "terraform" {
+  vpc_id = "${aws_vpc.terraform.id}"
+  cidr_block = "10.0.0.0/16"
 }
